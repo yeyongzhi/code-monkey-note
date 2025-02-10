6a4789bdd18601fab1b9114d605ab58bcfe5d18a
yeyongzhi <18258475212m@sina.cn>
2025-02-10 14:41:31 +0800

    feat: 提交部署

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 1023,
+  },
   plugins: [vue()],
-})
+});
