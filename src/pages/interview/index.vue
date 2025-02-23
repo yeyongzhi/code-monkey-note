352c9b1dd759261caa623d6d0774953349238d61
Aurora <18258475212m@sina.cn>
2025-02-23 21:28:36 +0800

    feat: 功能升级 1.pdf文件预览

--- src/pages/interview/index.vue
+++ src/pages/interview/index.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 
+import { ref } from 'vue';
+import VuePdfEmbed from 'vue-pdf-embed'
+
+const pdfSrc = ref("/article/pdf/example.pdf")
+
 </script>
 
 <template>
-    <div class="app_container">这里啥也没有</div>
+    <div class="app_container">
+        <VuePdfEmbed :source="pdfSrc" />
+    </div>
 </template>
 
 <style scoped lang="scss"></style>
