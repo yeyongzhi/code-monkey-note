348d6211ae6754724c358aced05d7108fd180b54
Aurora <18258475212m@sina.cn>
2025-11-01 12:03:57 +0800

    feat: 文章完善

---
+++ src/components/FluentIcon/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import * as Fluent from '@vicons/fluent'
+const { name } = defineProps({
+    name: { type: String, required: true, default: "" },
+    size: { type: Number, required: true, default: 25 },
+    color: { type: String, required: false, default: '' },
+})
+const icon = computed(() => {
+    return (Fluent as any)[name]
+})
+</script>
+
+<template>
+    <n-icon :size="size" :color="color">
+        <component :is="icon" />
+    </n-icon>
+</template>
+
+<style scoped lang="scss"></style>
